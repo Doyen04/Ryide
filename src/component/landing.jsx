ef65e508346a1af6c09254818d9874b0d97daff2
Doyen04 <solaopeyemi93@gmail.com>
2024-10-28 22:14:49 +0100

    testing a new arrangement

--- src/component/landing.jsx
+++ src/component/landing.jsx
@@ -10,37 +10,39 @@ function Landing() {
 
     return (
         <div className="landing-view" >
-            <div className='landing-view-description'>
-                <div>
-                    <div className='main-description'>
-                        Your Smooth Ride, <br />Just a Tap Away
+            <section className='view-maincover'>
+                <div className='landing-view-description'>
+                    <div>
+                        <div className='main-description'>
+                            Your Smooth Ride, <br />Just a Tap Away
+                        </div>
+                        <div className='sub-description'>
+                            Experience hassle-free transportation with Ryide <br />
+                            Reliable rides, anytime, anywhere at an affordable price
+                        </div>
                     </div>
-                    <div className='sub-description'>
-                        Experience hassle-free transportation with Ryide <br />
-                        Reliable rides, anytime, anywhere at an affordable price
-                    </div>
-                </div>
-                <div className='appstore-buttons-container'>
-                    <div className='googleplay'>
-                        <img src={googlePlayIcon} alt="" />
-                        <aside>
-                            Download on the <br />
-                            <p> Google Play</p>
-                        </aside>
-                    </div>
-                    <div className='appleplay'>
-                        <img src={applePlayIcon} alt="" />
-                        <aside>
-                            Download on the <br />
-                            <p>Apple Play</p>
-                        </aside>
+                    <div className='appstore-buttons-container'>
+                        <div className='googleplay'>
+                            <img src={googlePlayIcon} alt="" />
+                            <aside>
+                                Download on the <br />
+                                <p> Google Play</p>
+                            </aside>
+                        </div>
+                        <div className='appleplay'>
+                            <img src={applePlayIcon} alt="" />
+                            <aside>
+                                Download on the <br />
+                                <p>Apple Play</p>
+                            </aside>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='landing-view-image' >
-                <img src={phonewithfloats} alt="" />
-            </div>
+                <div className='landing-view-image' >
+                    <img src={phonewithfloats} alt="" />
+                </div>
+            </section>
         </div>
     )
 }
