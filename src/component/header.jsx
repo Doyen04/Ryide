9c17947252b834ef91e8f7970e8c5200f52464e0
Doyen04 <solaopeyemi93@gmail.com>
2024-10-28 21:40:56 +0100

    made adjustment to the header style

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,22 +1,24 @@
 import logo from '../assets/svg/logo.svg'
 import '../styles/header.css'
 
-function Header(){
+function Header() {
     return (
         <div className='header'>
-            <div className='logo-container'>
-                <img src={logo} alt="" />
-                Ryide
-            </div>
-            <div className='navbar'>
-                <div>Home</div>
-                <div>About</div>
-                <div>Contact</div>
-                <div>Blog</div>
-            </div>
-            <div className='signup-button'>
-                Sign Up
-            </div>
+            <section className='main-cover'>
+                <div className='logo-container'>
+                    <img src={logo} alt="" />
+                    Ryide
+                </div>
+                <div className='navbar'>
+                    <div>Home</div>
+                    <div>About</div>
+                    <div>Contact</div>
+                    <div>Blog</div>
+                </div>
+                <div className='signup-button'>
+                    Sign Up
+                </div>
+            </section>
 
         </div>
     )
