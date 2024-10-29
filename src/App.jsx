7948fb7091efc18bd5810dc25f62f4739aa7c365
Doyen04 <solaopeyemi93@gmail.com>
2024-10-29 20:50:26 +0100

    made header mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App(){
     return(
         <div className="body">
             <Header/>
-            <Landing/>
+            {/* <Landing/>
             <Body/>
-            <Footer/>
+            <Footer/> */}
         </div>
     )
 }
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,7 +1,17 @@
+
+
 import logo from '../assets/svg/logo.svg'
 import '../styles/header.css'
 
 function Header() {
+    function toggleButton(ev){
+        ev.target.style.backgroundColor = 'red'
+        document.getElementsByClassName('navbar')[0].classList.toggle('show')
+        document.getElementsByClassName('signup-button')[0].classList.toggle('show-button')
+        setTimeout(() => {
+            ev.target.style.backgroundColor = 'white'
+        }, 1000)
+    }
     return (
         <div className='header'>
             <section className='main-cover'>
@@ -18,6 +28,9 @@ function Header() {
                 <div className='signup-button'>
                     Sign Up
                 </div>
+                <button className='toggle' onClick={toggleButton}>
+                    | | |
+                </button>
             </section>
 
         </div>
